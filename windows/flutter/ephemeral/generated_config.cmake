fe80325a0bbf1abb649b3e115bc790295832f789
Alis <dubrovskaya.alis@yandex.ru>
2025-12-17 18:00:13 +0300

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "E:\\Langs\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "F:\\TOP College\\Mob DEV\\Coffe APP\\app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.1.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=E:\\Langs\\flutter"
+  "PROJECT_DIR=F:\\TOP College\\Mob DEV\\Coffe APP\\app"
+  "FLUTTER_ROOT=E:\\Langs\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=F:\\TOP College\\Mob DEV\\Coffe APP\\app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=F:\\TOP College\\Mob DEV\\Coffe APP\\app"
+  "FLUTTER_TARGET=F:\\TOP College\\Mob DEV\\Coffe APP\\app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzguMw==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049MTkwNzRkMTJmNw==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049MTNlNjU4NzI1ZA==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMC4x"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=F:\\TOP College\\Mob DEV\\Coffe APP\\app\\.dart_tool\\package_config.json"
+)
